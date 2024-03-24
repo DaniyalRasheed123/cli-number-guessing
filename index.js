fb10a1a8fa350d9857bb3bc4da2dfc21e14b22f8
DaniyalRasheed123 <164622956+DaniyalRasheed123@users.noreply.github.com>
2024-03-24 15:48:13 +0500

    created number guessing hame

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) Computer will generate number 
+// 2) user input number of guess
+// 3) compare user input with computer generate number and show result
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "Please guesss a number between 1-6",
+    },
+]);
+console.log(answers);
+if (answers.userGussedNumber === randomNumber) {
+    console.log("Congratulation your guessed number is correct");
+}
+else {
+    console.log("you guessed a wrong number");
+}
+;
